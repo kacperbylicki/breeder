82ad40fc2810e78ab1972b2f276751fd8fe404b0
Kacper Bylicki <60986172+kacperbylicki@users.noreply.github.com>
2022-07-03 20:25:34 +0200

    feat: add s3 bucket terraform configuration

--- infrastructure/apps.tf
+++ infrastructure/apps.tf
@@ -1,29 +1,13 @@
 resource "heroku_app" "breeder_backend_production" {
-  name   = "${var.heroku_backend_production_app}"
-  region = "${var.heroku_region}"
+  name   = var.heroku_backend_production_app
+  region = var.heroku_region
 
-  # config_vars = {
-  #     FILESYSTEMS_DEFAULT = "${var.filesystems_default}"
-  #     FILESYSTEMS_S3_KEY = "${aws_iam_access_key.s3_rw.id}"
-  #     FILESYSTEMS_S3_BUCKET = "${var.filesystems_s3_bucket}"
-  #     FILESYSTEMS_S3_REGION = "${var.filesystems_s3_region}"
-  #     FILESYSTEMS_S3_SECRET = "${aws_iam_access_key.s3_rw.secret}"
-  # }
-
-  buildpacks = "${var.heroku_app_buildpacks}"
+  buildpacks = var.heroku_app_buildpacks
 }
 
 resource "heroku_app" "breeder_frontend_production" {
-  name   = "${var.heroku_frontend_production_app}"
-  region = "${var.heroku_region}"
+  name   = var.heroku_frontend_production_app
+  region = var.heroku_region
 
-  # config_vars = {
-  #     FILESYSTEMS_DEFAULT = "${var.filesystems_default}"
-  #     FILESYSTEMS_S3_KEY = "${aws_iam_access_key.s3_rw.id}"
-  #     FILESYSTEMS_S3_BUCKET = "${var.filesystems_s3_bucket}"
-  #     FILESYSTEMS_S3_REGION = "${var.filesystems_s3_region}"
-  #     FILESYSTEMS_S3_SECRET = "${aws_iam_access_key.s3_rw.secret}"
-  # }
-
-  buildpacks = "${var.heroku_app_buildpacks}"
+  buildpacks = var.heroku_app_buildpacks
 }
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,3 +1,7 @@
 output "production_git_url" {
-  value = "${heroku_app.breeder_backend_production.git_url}"
+  value = heroku_app.breeder_backend_production.git_url
+}
+
+output "breeder_image_bucket_name" {
+  value = aws_s3_bucket.breeder_image_bucket.id
 }
