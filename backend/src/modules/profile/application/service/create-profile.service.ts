72a9c0e41cb22aa3c76f79be7887eec53297bc38
Kacper Bylicki <60986172+kacperbylicki@users.noreply.github.com>
2022-07-04 20:12:30 +0200

    chore: add log to uploaded avatar

--- backend/src/modules/profile/application/service/create-profile.service.ts
+++ backend/src/modules/profile/application/service/create-profile.service.ts
@@ -34,6 +34,8 @@ export class CreateProfileService implements BaseService<CreateProfilePayload, P
       ? await this.uploadImageService.execute({ data: avatar, filename: account.uuid })
       : null;
 
+    console.log(uploadedAvatar);
+
     const currentDate = dayjs();
     const age = currentDate.diff(data?.dateOfBirth, "year");
 
